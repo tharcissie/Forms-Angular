837c0d85858f541ac83f98301cde5ca1e36eb96e
tharcissie <tharcissieidufashe@gmail.com>
2023-01-13 11:01:51 +0200

    added form

---
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-settings-form',
+  templateUrl: './user-settings-form.component.html',
+  styleUrls: ['./user-settings-form.component.css']
+})
+export class UserSettingsFormComponent {
+
+}
