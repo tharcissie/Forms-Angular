fd86443ba2c090b030c942b4f2c809d8866c9755
tharcissie <tharcissieidufashe@gmail.com>
2023-01-13 17:55:01 +0200

    3rd part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+import { ButtonsModule } from 'ngx-bootstrap/buttons'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { RatingModule } from 'ngx-bootstrap/rating';
+
 
 import { AppComponent } from './app.component';
 import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
@@ -12,7 +18,13 @@ import { UserSettingsFormComponent } from './user-settings-form/user-settings-fo
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    ButtonsModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    BrowserAnimationsModule,
+    RatingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { DataService } from '../data/data.service';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
   styleUrls: ['./user-settings-form.component.css']
 })
-export class UserSettingsFormComponent {
+export class UserSettingsFormComponent implements OnInit {
 
+  originalUserSettings: UserSettings = {
+    name: '',
+    emailOffers: true,
+    interfaceStyle: '',
+    subscriptionType: 'Annual',
+    notes: 'Here are some notes...'
+  };
+
+
+  postError = false;
+  postErrorMessage = '';
+  subscriptionTypes!: Observable<string[]>;
+
+  max = 10;
+  rate = 7;
+  isReadonly = true;
+
+  userSettings: UserSettings = { ...this.originalUserSettings }
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.subscriptionTypes = this.dataService.getSubscriptionType();
+  }
+
+  onHttpError(errorResponse: any) {
+    console.log(errorResponse)
+    this.postError = true;
+    this.postErrorMessage = errorResponse.error.errorMessage
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value)
+    if (form.valid)
+      this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+        result => console.log(result),
+        error => this.onHttpError(error)
+      )
+
+    else {
+      this.postError = true;
+      this.postErrorMessage = "please fix the above errors"
+    }
+  }
+
+  onBlur(field: NgModel) {
+    console.log(field)
+  }
 }
